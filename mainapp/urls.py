1c50d8e804966bad941118613a839eee64e24afc
OBARGwin <115862237+OBARGwin@users.noreply.github.com>
2024-08-26 12:31:53 +0500

    Update urls.py

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns =[
+  path('', GetTokenApiView.as_view()),
   path('get_token', GetTokenApiView.as_view()),
   path('goods', list_goods),
   path('new_good', new_good),
